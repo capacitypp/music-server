f28140a1010f6f68df81ed783f457d0734f8e7ba
capacitypp <aoi0x4c4f5645@gmail.com>
2016-09-08 18:11:04 +0900

    [add] halt on reading file error

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,11 @@ int main(int argc, char** argv)
 	ALuint source;
 	alutInit(&argc, argv);
 	buffer = alutCreateBufferFromFile("data/sample.wav");
+	if (buffer == AL_NONE) {
+		cerr << "alutCreateBufferFromFile() failed." << endl;
+		alutExit();
+		return 1;
+	}
 	alGenSources(1, &source);
 	alSourcei(source, AL_BUFFER, buffer);
 	alSourcePlay(source);
