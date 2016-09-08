266f68ba6f7c888a520d3071b6dd75fcb58d3c02
capacitypp <aoi0x4c4f5645@gmail.com>
2016-09-08 17:55:55 +0900

    [add] play a wav file

--- Main.cpp
+++ Main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char** argv)
 	ALuint buffer;
 	ALuint source;
 	alutInit(&argc, argv);
-	buffer = alutCreateBufferHelloWorld();
+	buffer = alutCreateBufferFromFile("data/sample.wav");
 	alGenSources(1, &source);
 	alSourcei(source, AL_BUFFER, buffer);
 	alSourcePlay(source);
-	alutSleep(3);
+	alutSleep(10);
 	alutExit();
 
 	return 0;
